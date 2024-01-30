bca7e68b35624d79d843c0951c5168b04b1862d9
Saksham <sakshamverma7844@gmail.com>
2024-01-30 20:42:27 +0530

    navbar done

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,46 @@
 import './Navbar.scss'
-import { FaWhatsapp, FaPhoneAlt } from "react-icons/fa";
+import { FaWhatsapp, FaPhoneAlt } from 'react-icons/fa'
+import data from '../../utils/Category.json'
+import {
+  MdOutlineKeyboardArrowDown,
+  MdKeyboardArrowRight,
+} from 'react-icons/md'
+
+import { RiArrowUpSLine } from 'react-icons/ri'
+import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='nav__container'>
-       <div className="nav__contact__details">
+    <nav className="nav__container">
+      <section className="nav__contact__details">
         <div>
-          <FaWhatsapp/>
+          <FaWhatsapp />
           <p>WhatsApp Your Order</p>
         </div>
         <div>
-          <FaPhoneAlt/>
+          <FaPhoneAlt />
           <p>+9111-41087447</p>
         </div>
-       </div>
+      </section>
+
+      <section className="nav__category">
+        {data?.map((item, index) => (
+          
+            <div className="category__title" key={index}>
+              {item?.title}
+              {<RiArrowUpSLine className="icon__up" color="black" size={20} />}
+              {
+                <MdOutlineKeyboardArrowDown
+                  color="black"
+                  size={20}
+                  className="icon__down"
+                />
+              }
+            </div>
+
+            
+        ))}
+      </section>
     </nav>
   )
 }
